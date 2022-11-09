bbcf7991f380ec42cb2737bd866982733b3b8729
cthacker-udel <cthacker@udel.edu>
2022-11-08 21:00:30 -0500

    [aboutme] Added basic projects to dates

---
+++ modules/Projects/ProjectContainer/index.ts
@@ -0,0 +1,1 @@
+export { ProjectContainer } from "./ProjectContainer.component";
---
+++ modules/Projects/ProjectYear.ts
@@ -0,0 +1,9 @@
+/* eslint-disable no-magic-numbers -- disabled */
+/* eslint-disable no-shadow -- disabled */
+/* eslint-disable no-unused-vars -- disabled */
+
+export enum ProjectYear {
+	TWENTY_TWENTY = 0,
+	TWENTY_TWENTY_ONE = 1,
+	TWENTY_TWENTY_TWO = 2,
+}
