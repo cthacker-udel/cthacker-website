f9196fd51606f827ea9f8abf5f2cae7cc2669c7b
cthacker-udel <cthacker@udel.edu>
2022-11-15 05:45:43 -0500

    [schoolcarousel] Added redirect functionality to images

--- modules/WorkHistory/WorkHistorySection/WorkHistorySection.component.tsx
+++ modules/WorkHistory/WorkHistorySection/WorkHistorySection.component.tsx
@@ -1,4 +1,6 @@
+/* eslint-disable @typescript-eslint/no-floating-promises -- disabled, known bug */
 import Image from "next/image";
+import { useRouter } from "next/router";
 import React from "react";
 
 import workHistorySectionStyles from "./WorkHistorySection.module.css";
@@ -10,6 +12,7 @@ type WorkHistorySectionProperties = {
 	endDate?: Date;
 	jobLocation: string;
 	jobTitle: string;
+	imgHrefLink?: string;
 	imgLink?: string;
 	startDate?: Date;
 };
@@ -34,65 +37,81 @@ export const WorkHistorySection = ({
 	endDate,
 	jobLocation,
 	jobTitle,
+	imgHrefLink,
 	imgLink,
 	startDate,
-}: WorkHistorySectionProperties): JSX.Element => (
-	<div
-		className={`d-flex flex-row justify-content-around w-100 mb-4 ${workHistorySectionStyles.section}`}
-	>
-		{imgLink && (
-			<div className="d-flex flex-column">
-				<Image
-					alt="work_history_section_link"
-					className={`${workHistorySectionStyles.job_img} shadow`}
-					height="200"
-					src={imgLink}
-					width="200"
-				/>
-				{jobTitle && (
-					<div
-						className={`text-center ${workHistorySectionStyles.img_subtitle}`}
-					>
-						{jobTitle}
-					</div>
-				)}
-				{jobLocation && (
-					<div
-						className={`text-center ${workHistorySectionStyles.img_subtitle}`}
-					>
-						{jobLocation}
-					</div>
-				)}
-				{companyName && (
-					<div
-						className={`text-center ${workHistorySectionStyles.img_subtitle}`}
-					>
-						{companyName}
-					</div>
-				)}
-				{current ? (
-					<div
-						className={`badge bg-primary ${workHistorySectionStyles.current_badge} mx-auto mt-1`}
-					>
-						{"Current"}
-					</div>
-				) : (
-					<div
-						className={`${workHistorySectionStyles.start_end_date} text-center`}
-					>
-						{startDate &&
-							endDate &&
-							`${dateifyDate(startDate)} - ${dateifyDate(
-								endDate,
-							)}`}
-					</div>
-				)}
-			</div>
-		)}
+}: WorkHistorySectionProperties): JSX.Element => {
+	const router = useRouter();
+
+	/**
+	 *
+	 * @param route
+	 */
+	const imgClick = (route: string): void => {
+		router.push(route);
+	};
+
+	return (
 		<div
-			className={`${workHistorySectionStyles.description} d-flex flex-column justify-content-center text-wrap`}
+			className={`d-flex flex-row justify-content-around w-100 mb-4 ${workHistorySectionStyles.section}`}
 		>
-			{description}
+			{imgLink && (
+				<div className="d-flex flex-column">
+					<Image
+						alt="work_history_section_link"
+						className={`${workHistorySectionStyles.job_img} shadow`}
+						height="200"
+						onClick={(): void => {
+							imgClick(imgHrefLink ?? "#");
+						}}
+						src={imgLink}
+						width="200"
+					/>
+					{jobTitle && (
+						<div
+							className={`text-center ${workHistorySectionStyles.img_subtitle}`}
+						>
+							{jobTitle}
+						</div>
+					)}
+					{jobLocation && (
+						<div
+							className={`text-center ${workHistorySectionStyles.img_subtitle}`}
+						>
+							{jobLocation}
+						</div>
+					)}
+					{companyName && (
+						<div
+							className={`text-center ${workHistorySectionStyles.img_subtitle}`}
+						>
+							{companyName}
+						</div>
+					)}
+					{current ? (
+						<div
+							className={`badge bg-primary ${workHistorySectionStyles.current_badge} mx-auto mt-1`}
+						>
+							{"Current"}
+						</div>
+					) : (
+						<div
+							className={`${workHistorySectionStyles.start_end_date} text-center`}
+						>
+							{startDate &&
+								endDate &&
+								`${dateifyDate(startDate)} - ${dateifyDate(
+									endDate,
+								)}`}
+						</div>
+					)}
+				</div>
+			)}
+			<div
+				className={`${workHistorySectionStyles.description} d-flex flex-column justify-content-center text-wrap`}
+			>
+				{description}
+			</div>
 		</div>
-	</div>
-);
+	);
+};
--- modules/WorkHistory/sections/2022/2022Fall.tsx
+++ modules/WorkHistory/sections/2022/2022Fall.tsx
@@ -7,6 +7,7 @@ export const Fall2022 = [
 		companyName="University of Delaware"
 		current
 		description="Taught and supervised multiple different programming courses. Ensured grading and delivery of information was top quality; consistently. Taught students Java, Python, and C on an introductory level; while introducing them to new and complicated topics."
+		imgHrefLink="https://udel.edu"
 		imgLink="/udcampus.jpg"
 		jobLocation="Newark, DE"
 		jobTitle="Teaching Assistant"
@@ -16,6 +17,7 @@ export const Fall2022 = [
 		companyName="University of Delaware"
 		current
 		description="Involved in a research project with a team of colleagues under the supervision of Professor Yarrington, one that was tailored towards making assignment timelines more accessible to neuro-divergent students."
+		imgHrefLink="https://udel.edu"
 		imgLink="/udcampus2.jpg"
 		jobLocation="Newark, DE"
 		jobTitle="Research Assistant"
@@ -25,6 +27,7 @@ export const Fall2022 = [
 		companyName="Quantum Strategies LLC"
 		current
 		description="Developing greenfield projects in React and NodeJS. Constructing a backend and front-end interface for flawless communication. Involving cross-team communication and management to get projects done to meet strict deadlines. Scrum format."
+		imgHrefLink="https://qs2500.com/"
 		imgLink="/qslogo.jpg"
 		jobLocation="Glen Mills, PA"
 		jobTitle="Junior Full-Stack Developer"
@@ -34,6 +37,7 @@ export const Fall2022 = [
 		companyName="University of Delaware"
 		current
 		description="Personal tutor for students starting introductory computer science coursework and helped cultivate and improve their understanding of the subject. Lectured programming languages ranging from OOP to Bash Scripting, and all students who signed up for help. I converted their overall grades from a D+ to a B+ with the help I provided playing a factor."
+		imgHrefLink="https://udel.edu"
 		imgLink="/udcampus3.jpg"
 		jobLocation="Newark, DE"
 		jobTitle="Computer Science Tutor"
@@ -43,6 +47,7 @@ export const Fall2022 = [
 		companyName="TherapyNotes LLC"
 		description="Worked in the Advance Team, one of which was working on a greenfield project involving a new monumental feature to enhance the current application ecosphere. Coded in C#, React, SQL. Utilized Azure Service Bus, Octopus Deployment, ElasticSearch, and Jira for project management."
 		endDate={new Date(2022, 9, 28)}
+		imgHrefLink="https://www.therapynotes.com/"
 		imgLink="/tn.png"
 		jobLocation="Horsham, PA"
 		jobTitle="Co-Op Full-Stack Developer"
