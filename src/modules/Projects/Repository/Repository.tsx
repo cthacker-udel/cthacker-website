1fd06de61ed9f52cc0c60fcd16dc6e36f121a9cb
cthacker-udel <cthacker@udel.edu>
2023-07-07 14:52:39 -0400

    [updates] Added hover effect for repositories

--- src/modules/Projects/Repository/Repository.tsx
+++ src/modules/Projects/Repository/Repository.tsx
@@ -5,6 +5,7 @@ import React from "react";
 import { useRepoLanguages } from "@/hooks/useRepoLanguages";
 
 import type { Repo } from "../helpers";
+import otherStyles from "../Projects.module.css";
 import styles from "./Repository.module.css";
 
 type RepositoryProperties = Repo;
@@ -18,6 +19,46 @@ type RepositoryProperties = Repo;
 export const Repository = ({ ...rest }: RepositoryProperties): JSX.Element => {
     const { languages } = useRepoLanguages(rest.owner, rest.name);
 
+    const mouseEnter = React.useCallback(
+        (event: React.MouseEvent<HTMLDivElement>) => {
+            console.log("entering");
+            const { target } = event;
+            if (target !== undefined) {
+                const convertedTarget = target as HTMLDivElement;
+                if (
+                    !convertedTarget.className.includes(
+                        ` ${otherStyles.currently_selected}`,
+                    )
+                ) {
+                    convertedTarget.className += ` ${otherStyles.currently_selected}`;
+                }
+            }
+        },
+        [],
+    );
+
+    const mouseLeave = React.useCallback(
+        (event: React.MouseEvent<HTMLDivElement>) => {
+            console.log("leaving");
+            const { target } = event;
+            if (target !== undefined) {
+                const convertedTarget = target as HTMLDivElement;
+                if (
+                    convertedTarget.className.includes(
+                        ` ${otherStyles.currently_selected}`,
+                    )
+                ) {
+                    convertedTarget.className =
+                        convertedTarget.className.replace(
+                            ` ${otherStyles.currently_selected}`,
+                            "",
+                        );
+                }
+            }
+        },
+        [],
+    );
+
     return (
         <div
             className={styles.repository}
@@ -30,6 +71,8 @@ export const Repository = ({ ...rest }: RepositoryProperties): JSX.Element => {
                     window.open(rest.html_url, "_newtab");
                 }
             }}
+            onMouseEnter={mouseEnter}
+            onMouseLeave={mouseLeave}
         >
             <div className={styles.name}>{rest.name}</div>
             <div className={styles.date}>
